6c75808da747727473dd4faf44a135043c8c66ca
Egor Tolstoy <igrekde@gmail.com>
2020-01-17 11:13:16 +0300

    Updated data model

---
+++ Backend/src/main/kotlin/ru/podlodka/backend/models/Selection.kt
@@ -0,0 +1,15 @@
+package ru.podlodka.backend.models
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Selection(
+        @Id var id: String,
+        // Имя подборки выпусков
+        val name: String,
+        // Ссылка на обложку подборки
+        val imageUrl: String,
+        // Список выпусков в подборке
+        val episodes: List<ShowEpisode>
+);
