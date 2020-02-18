f35e063329bbc5fc1a11b4dae9aa964c3518d2b7
Egor Tolstoy <igrekde@gmail.com>
2020-02-18 11:57:58 +0300

    Implemented cache-first categories policy

--- SharedCode/src/commonMain/kotlin/podlodka.mpp/DatabaseHelper.kt
+++ SharedCode/src/commonMain/kotlin/podlodka.mpp/DatabaseHelper.kt
@@ -19,32 +19,14 @@ class DatabaseHelper {
         sqlDriver.close()
     }
 
-    fun selectAllItems(): Query<podlodka.SqlEpisodeCategory> = db.podlodkaDatabaseQueries.selectAll()
+    fun selectAllEpisodeCategories(): Query<podlodka.SqlEpisodeCategory> = db.podlodkaDatabaseQueries.selectAll()
 
     fun insertEpisodeCategories(categories: List<EpisodeCategory>) {
         db.transaction {
             categories.forEach { category ->
-                db.podlodkaDatabaseQueries.insertEpisodeCategory(category.id, category.name, category.emoji)
+                db.podlodkaDatabaseQueries.insertEpisodeCategory(category.id, category.name, category.emoji, category.episodeIds.joinToString(separator = ","))
             }
         }
     }
 
-//    suspend fun insertBreeds(breedNames: List<String>) = withContext(Dispatchers.Default) {
-//        dbRef.transaction {
-//            breedNames.forEach { name ->
-//                dbRef.tableQueries.insertBreed(null, name, 0)
-//            }
-//        }
-//    }
-//
-//    suspend fun selectById(id: Long): Query<Breed> =
-//        withContext(Dispatchers.Default) { dbRef.tableQueries.selectById(id) }
-//
-//    suspend fun deleteAll() = withContext(Dispatchers.Default) {
-//        dbRef.tableQueries.deleteAll()
-//    }
-//
-//    suspend fun updateFavorite(breedId: Long, favorite: Boolean) = withContext(Dispatchers.Default) {
-//        dbRef.tableQueries.updateFavorite(favorite.toLong(), breedId)
-//    }
 }
--- SharedCode/src/commonMain/kotlin/podlodka.mpp/module/episodeDetail/EpisodeDetailViewModel.kt
+++ SharedCode/src/commonMain/kotlin/podlodka.mpp/module/episodeDetail/EpisodeDetailViewModel.kt
@@ -8,18 +8,54 @@ class EpisodeDetailViewModel {
     private val service = CategoryService()
     fun getCategories(callback: (List<EpisodeCategory>) -> Unit) {
         val helper = DatabaseHelper()
+        val savedCategories = getSavedCategories()
 
+        if (savedCategories != null) {
 
-        service.getCategories {
-            if (it != null) {
-                helper.insertEpisodeCategories(it)
+            service.getCategories {
+                if (it != null) {
+                    helper.insertEpisodeCategories(it)
+                }
             }
-            val query = helper.selectAllItems()
-            val arr = query.executeAsList()
-            print(arr)
-            if (it != null) {
-                callback(it)
+            callback(savedCategories)
+            return
+        } else {
+            service.getCategories {
+                if (it != null) {
+                    helper.insertEpisodeCategories(it)
+                }
+
+                val savedCategories = getSavedCategories()
+
+                if (savedCategories != null) {
+                    callback(savedCategories)
+                }
             }
         }
+
+
+    }
+
+    private fun getSavedCategories(): List<EpisodeCategory>? {
+        val helper = DatabaseHelper()
+        val query = helper.selectAllEpisodeCategories()
+        val savedCategories = query.executeAsList()
+
+        if (savedCategories == null) {
+            return null
+        }
+
+        val result = mutableListOf<EpisodeCategory>()
+        savedCategories.forEach {
+            val category = EpisodeCategory(
+                it.id,
+                it.name,
+                it.emoji,
+                it.episodeIds.split(",")
+            )
+            result += category
+        }
+
+        return result
     }
 }
